cb7a45cf29bd5a5a39484d02dfa7b08a97fdef6e
Melissa Johnson <melissaleighjohnson@gmail.com>
2019-11-10 04:10:43 +0000

    Client-side nav not quite working yetwq

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 import * as animations from './animations.js';
+import * as navigation from './navigation.js';
 
 
 $(document).ready(function(){
+  console.log("HELLOW WORLD");
+  navigation.init();
   animations.setUp();
 });
