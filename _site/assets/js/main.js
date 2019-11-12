2621268dd904136db8737c2bdd7adce4830672cd
Melissa Johnson <melissaleighjohnson@gmail.com>
2019-11-11 23:02:35 -0800

    Untrack _site folder

--- _site/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-import * as animations from './animations.js';
-import * as navigation from './navigation.js';
-
-
-$(document).ready(function(){
-  console.log("HELLOW WORLD");
-  navigation.init();
-  animations.setUp();
-});
